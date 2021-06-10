bf0c610649fdb78f1880dc1350a016c4d77d1cf4
Daniel Lienert <daniel@lienert.cc>
2021-06-10 20:26:28 +0200

    TASK: Remove unneeded comments

--- Migrations/Mysql/Version20210607194130.php
+++ Migrations/Mysql/Version20210607194130.php
@@ -15,14 +15,12 @@ final class Version20210607194130 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('ALTER TABLE punktde_form_persistence_domain_model_formdata ADD sitename VARCHAR(255) NOT NULL, LONGTEXT NOT NULL COMMENT \'(DC2Type:flow_json_array)\'');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('ALTER TABLE punktde_form_persistence_domain_model_formdata DROP sitename, DROP contentdimensions');
     }
